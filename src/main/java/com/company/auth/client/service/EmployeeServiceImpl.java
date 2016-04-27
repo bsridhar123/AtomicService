1157244700c3e7c62f220a9443a1f6803db892bc
bsridhar123 <bs.tech.chn@gmail.com>
2016-04-27 13:00:33 +0530

    Update EmployeeServiceImpl.java

--- src/main/java/com/company/auth/client/service/EmployeeServiceImpl.java
+++ src/main/java/com/company/auth/client/service/EmployeeServiceImpl.java
@@ -4,6 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.company.auth.client.exception.DuplicateResourceException;
+import com.company.auth.client.exception.ResourceNotFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 import com.company.auth.client.model.Employee;
 
 @Component
@@ -11,18 +18,42 @@ public class EmployeeServiceImpl implements EmployeeService{
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 	
+	private static Map<String, Employee> employeeMap=new HashMap<String, Quote>();
+	
+	static {
+		employeeMap.put("test", new Employee("test","testEmployee"));
+	}
+	
 	 
 	public Employee getEmployee(String id) throws Exception{
 		LOGGER.info("Entering");
     	LOGGER.info("Received Id  is :" + id);
-    	return new Employee("1","Sridhar");
+    	Employee employee=(Employee) employeeMap.get(id);
+    	if(null!=employee){
+    		LOGGER.info("Fetched Employee Successfully");
+    		LOGGER.info("Leaving getEmployee");
+    		return (Employee) employeeMap.get(id);
+    	}else{
+    		throw new ResourceNotFoundException(
+					"Invalid Employee no: " + id + " passed as input ");
+    	}
+    	return employee;
 	}
 	
 	
 	
 	 public String createEmployee(Employee employee) throws Exception {
-		LOGGER.info("Entering");
+LOGGER.info("Entering");
     	LOGGER.info("Received Employee Object  is :" + employee.toString());
+    	
+    	Employee eEmployee=employeeMap.get(employee.getEmpno());
+    	
+    	if(null!=eEmployee && eEmployee.getEmpno().equalsIgnoreCase(employee.getEmpno())){
+    		throw new DuplicateResourceException(
+					"Passed Quote No:" + employee.getEmpno() + " Already Exists");
+    	}
+    	
+    	employeeMap.put(employee.getEmpno(),employee);
     	LOGGER.info("Saved Employee Successfully.");
     	LOGGER.info("Leaving createEmployee");
         return employee.getEmpno();
